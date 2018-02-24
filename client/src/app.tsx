6469bfa7e85320a7e2276fc5c07c16d08fb4dfc7
Nick Van Doorn <vandoorn.nick@gmail.com>
2018-02-24 13:55:34 -0800

    Render a bear

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react'
 import './app.css'
 
+const bear = require('./bear.jpg')
 const logo = require('./logo.svg')
 
-class App extends React.Component {
+class App extends React.Component<any, any> {
   render() {
+    console.log(this.props)
+    const myImg = <img src={bear} />
     return (
       <div className="App">
         <header className="App-header">
+          {myImg}
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
