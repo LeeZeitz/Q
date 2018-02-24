d9d59b3cf0924a2560486c8c7707ae0a9d75be21
Nick Van Doorn <vandoorn.nick@gmail.com>
2018-02-24 15:03:02 -0800

    Bootstrap header

---
+++ client/src/components/header/header.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import './header.css'
+
+export default (props: any) => {
+  return (
+    <div className='header' onClick={props.onClick}>
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
