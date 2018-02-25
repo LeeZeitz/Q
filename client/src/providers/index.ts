c8067aadb5a164c7428659401d77b8c7d2c5664d
Nick Van Doorn <vandoorn.nick@gmail.com>
2018-02-24 17:20:51 -0800

    Add http example

--- client/src/providers/index.ts
+++ client/src/providers/index.ts
@@ -1,8 +1,31 @@
-export default (host: string, port: number = 5000) => {
+import axios from 'axios'
+
+// Make the http lib injectable such that
+// this can be tested easily
+export default (host: string, port: number = 5000, http: any = axios) => {
+  const baseUrl = `${host}:${port}`
+  // some http req
+  const g = () => http.get(baseUrl)
   return {
-    hello () {
-      console.log('hello world from the provider')
-      console.log(`the current provider is on host ${host} and port ${port}`)
+    // await can only be used in functions
+    // delcared "async".
+    async hello () {
+      // use axios as a promise or with async await
+      const res: any = await g()
+      console.log(`do stuff with ${res}`)
+      return res
+
+    },
+    // keep in mind this is a plain
+    // object, not a class, so we need
+    // the , separator
+    helloPromise () {
+      return new Promise((resolve, reject) => {
+        g().then((res: any) => {
+          console.log(`do stuff with ${res}`)
+          resolve(res)
+        })
+      })
     }
   }
 }
